3ab46b857e5b03a7811b40420e6102e6d60ab7f0
andersonkmi <andersonkmi@acm.org>
2014-04-23 22:55:33 -0300

    Initial commit

---
+++ MyCertManager/CryptoServiceProviderDetector/CryptoServiceProviderInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoServiceProviderDetector
+{
+    public class CryptoServiceProviderInformation
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public String Editor
+        {
+            get;
+            set;
+        }
+
+        public String Version
+        {
+            get;
+            set;
+        }
+
+        public String Pkcs11LibraryPath
+        {
+            get;
+            set;
+        }
+    }
+}
