ad1bb96fb119caaff9dc040f5f6745d871165dfb
andersonkmi <andersonkmi@acm.org>
2014-05-06 21:03:57 -0300

    Finished ICP Brasil certificate class

--- MyCertManager/CertificateCore/BrPkiCertificate.cs
+++ MyCertManager/CertificateCore/BrPkiCertificate.cs
@@ -20,7 +20,81 @@ namespace CertificateCore
         private String votingZone;
         private String votingSection;
         private String votingLocation;
-        private String socialSecurityIdNumber;
+        private String socialSecurityNumber;
+        #endregion
+
+        #region Properties
+        public BrazilPKICertificateType BrazilPKICertType
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public String TaxpayerId
+        {
+            get { return taxpayerId; }
+            set { taxpayerId = value; }
+        }
+
+        public String FederalTaxId
+        {
+            get { return federalTaxId; }
+            set { federalTaxId = value; }
+        }
+
+        public String CertificateRep
+        {
+            get { return certificateRep; }
+            set { certificateRep = value; }
+        }
+
+        public String CivilIdentification
+        {
+            get { return civilIdentification; }
+            set { civilIdentification = value; }
+        }
+
+        public String WorkerFundIdentification
+        {
+            get { return workerFundIdentification; }
+            set { workerFundIdentification = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+
+        public String VoterIdNumber
+        {
+            get { return voterIdNumber; }
+            set { voterIdNumber = value; }
+        }
+
+        public String VotingZone
+        {
+            get { return votingZone; }
+            set { votingZone = value; }
+        }
+
+        public String VotingSection
+        {
+            get { return votingSection; }
+            set { votingSection = value; }
+        }
+
+        public String VotingLocation
+        {
+            get { return votingLocation; }
+            set { votingLocation = value; }
+        }
+
+        public String SocialSecurityNumber
+        {
+            get { return socialSecurityNumber; }
+            set { socialSecurityNumber = value; }
+        }
         #endregion
     }
 }
