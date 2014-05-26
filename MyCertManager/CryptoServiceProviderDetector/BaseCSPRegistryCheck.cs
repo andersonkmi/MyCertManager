48da5993d56f0ea7f81dcd39fdf038c6504d53a3
andersonkmi <andersonkmi@acm.org>
2014-05-26 08:17:18 -0300

    Modifications to current implementation and new files added

---
+++ MyCertManager/CryptoServiceProviderDetector/BaseCSPRegistryCheck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoServiceProviderDetector
+{
+    public abstract class BaseCSPRegistryCheck : ICSPRegistryCheck
+    {
+        private const string ProgramsRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+        public abstract CryptoServiceProviderInformation verifyInstallation();
+    }
+}
---
+++ MyCertManager/CryptoServiceProviderDetector/CSPDetector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoServiceProviderDetector
+{
+    public class CSPDetector
+    {
+        #region Internal attributes
+        private ISet<ICSPRegistryCheck> cspDetectors;
+        #endregion
+
+        #region Constructors
+        public CSPDetector()
+        {
+            this.cspDetectors = new HashSet<ICSPRegistryCheck>();
+        }
+
+        public CSPDetector(ISet<ICSPRegistryCheck> detectors)
+        {
+            this.cspDetectors = detectors;
+        }
+        #endregion
+
+        #region Methods
+        public ISet<CryptoServiceProviderInformation> detectCSPs()
+        {
+            ISet<CryptoServiceProviderInformation> results = new HashSet<CryptoServiceProviderInformation>();
+            return results;
+        }
+
+        private void executeDetectors(ISet<CryptoServiceProviderInformation> items)
+        {
+            foreach (ICSPRegistryCheck detector in cspDetectors)
+            {
+                items.Add(detector.verifyInstallation());
+            }
+        }
+        #endregion
+    }
+}
