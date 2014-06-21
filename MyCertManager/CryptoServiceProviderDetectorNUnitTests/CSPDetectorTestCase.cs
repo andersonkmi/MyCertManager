15fa85013a1e9b88fbabc718f07c7b6121c9e187
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2014-06-20 22:13:21 -0300

    Initial unit-test commit

---
+++ MyCertManager/CryptoServiceProviderDetectorNUnitTests/CSPDetectorTestCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CryptoServiceProviderDetector;
+
+namespace CryptoServiceProviderDetectorNUnitTests
+{
+    [TestFixture]
+    public class CSPDetectorTestCase
+    {
+        private CSPDetector detector;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.detector = new CSPDetector();
+        }
+
+        [Test]
+        public void TestDetectorEmptyDetectorsListOK()
+        {
+
+        }
+    }
+}
