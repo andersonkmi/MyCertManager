db92a776aba2a30fb5fba74da9d049d1b4194f2e
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2014-10-05 21:39:25 -0300

    refactoring

--- MyCertManager/CertificateCore/HashAlgorithm.cs
+++ MyCertManager/CertificateCore/HashAlgorithm.cs
@@ -8,9 +8,9 @@ namespace CertificateCore
 {
     public enum HashAlgorithm
     {
-        SHA1,
-        SHA256,
-        SHA384,
-        SHA512
+        Sha1,
+        Sha256,
+        Sha384,
+        Sha512
     }
 }
